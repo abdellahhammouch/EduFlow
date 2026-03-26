04489fe9e9647e45c5b027461de8332c96e27798
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-03-26 12:46:25 +0100

    Add enrollment repository interface

---
+++ app/Interfaces/Repositories/EnrollmentRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Interfaces\Repositories;
+
+use App\Models\Enrollment;
+use Illuminate\Support\Collection;
+
+interface EnrollmentRepositoryInterface
+{
+    public function create(array $data): Enrollment;
+
+    public function findByStudentAndCourse(int $studentId, int $courseId): ?Enrollment;
+
+    public function listByCourse(int $courseId): Collection;
+}
